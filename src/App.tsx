9cd25e52f0142454378a26494e58c76143b755e9
Your Name <you@example.com>
2022-03-31 11:39:33 +0700

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import { collection , getDocs } from 'firebase/firestore';
+import { collection , getDocs, addDoc,updateDoc,doc, deleteDoc } from 'firebase/firestore';
 import {db} from './firebase/config'
 import { useState, useEffect } from 'react';
  import './App.css';
-type userProp = {
 
+type useProp= {
+  name : string
+  age : number
+  id : string
 }
 function App() {
-  const [user,getUser] = useState<userProp[]>([]);
+  const [newName,setNewName] = useState("");
+  const [newAge,setNewAge] =useState(0);
+  const [user,getUser] = useState<any>([]);
   const usersCollectionRef =collection(db, "users");
-
-   useEffect(() => {
+    const updateUser =async (id :string,age :number) => {
+      const userDoc = doc(db,"users", id )
+      const newFieds = { age : age + 1}
+      await updateDoc(userDoc,newFieds)
+    }
+    const createUser = async () => {
+        await addDoc(usersCollectionRef,{name :newName ,age :newAge});
+    }
+    const deleteUser = async (id : string) => {
+      const userDoc = doc(db,"users",id);
+      await deleteDoc(userDoc)
+    }
+   useEffect(() => {  
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      const item = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      const item = data.docs.map((doc) => ({ ...doc.data(),id : doc.id }));
+      console.log(typeof item[0].id);
       getUser(item)
     };
     getUsers();
@@ -21,8 +38,21 @@ function App() {
   console.log(user)
   return (
     <div className="App">
-     
-    </div>
+        <input type="text" placeholder="Name..." onChange={(event) => {setNewName(event.target.value)}}></input>
+        <input type="number" placeholder="Age..." onChange={(event) => {setNewAge(Number(event.target.value))}}></input>
+      <button onClick = {createUser}> Create</button>
+        {user.map((item : useProp,index : number) => 
+       (
+         
+         <div key= {index}>
+          <h2 > {item.name}</h2> 
+          <h2 > {item.age}</h2>
+          <button onClick={() => { updateUser(item.id,item.age)}}>Increae Age</button>
+          <button onClick={()=> {deleteUser(item.id)}}>DeleteUser</button>
+         </div>
+      ))
+       }
+        </div>
   );
 }
 
